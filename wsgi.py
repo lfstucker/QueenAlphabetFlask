5e9b64817c59cb6f184a142caa31241cb2a3b9fa
Logan Stucker <stuck124@umn.edu>
2020-09-13 16:48:26 -0500

    should actually run now

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,25 @@
-from flask import Flask
-app = Flask(__name__)
+# Thinking base python app with bootstrap ect to run
+import requests
+import ssl
+import os
+import time
+import random
+import pytz
+import aiohttp
+from flask import Flask, render_template, request, json, jsonify, send_from_directory, url_for, redirect, session
+
+app = Flask(__name__,
+static_folder='static',
+static_url_path='/static',
+template_folder='templates')
+
+app.secret_key = os.urandom(12).hex()
+app.config['DEBUG'] = True
+app.logger.info('Started QA')
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Mollly!'
+def return_index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
